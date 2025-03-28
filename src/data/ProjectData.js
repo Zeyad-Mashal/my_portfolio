608f95ef661c06cd9e4eb84d9df994a5d7f9d36b
Mashal <zyadomar112@gmail.com>
2025-03-28 19:18:33 +0200

    first commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,117 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/ykte-feature-graphic.png",
+    title: "maro cares",
+    description:
+      "A cross-platform mobile game (in closed testing) where players try to find the missing pieces of a story.",
+    tech_stack: ["Node.js", "NextJs", "TailwindCSS", "Express.js"],
+    github_url: "",
+    demo_url: "https://marocares.com/",
+  },
+  {
+    img: "/suwalif.png",
+    title: "suwalif Tea",
+    description:
+      "An E-commerce website for selling tea and related products with dynamic categories.",
+    tech_stack: ["NextJS", "TailwindCSS", "NodeJs", "MongoDB"],
+    github_url: "",
+    demo_url: "https://suwalif.vercel.app/ar",
+  },
+  {
+    img: "/dashboard.png",
+    title: "Customize Dashboard",
+    description:
+      "A Customize Dashboard for testing systems such as: ERP, CRM, etc...",
+    tech_stack: ["React.js", "TailwindCSS"],
+    github_url: "",
+    demo_url: "https://osamamohamedatta.github.io/raqeb_dashboard/",
+  },
+  {
+    img: "/mira4k.png",
+    title: "Mira4k Movie tv",
+    description:
+      "A Movie Tv Website, Get top-rated TV customer service Channels.",
+    tech_stack: ["HTML", "CSS", "JavaScript", "Node.js"],
+    github_url: "",
+    demo_url: "https://mira4k.tv ",
+  },
+  {
+    img: "/p-p.png",
+    title: "Practice Papers",
+    description:
+      "An app to detect if a job posting is a scam or not using its description.",
+    tech_stack: ["React.js", "NodeJs", "MongoDB"],
+    github_url: "",
+    demo_url: "https://practice-papers.com",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+    name: "NextJs",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+    name: "Typescript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "tailwind.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
